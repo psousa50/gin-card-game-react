1cf73e7aae5f5397e350df02061aa8d550b15452
Pedro Sousa <pedronsousa@gmail.com>
2020-03-22 12:11:44 +0000

    Fix gitignore

--- src/gin-card-game/utils/misc.ts
+++
@@ -1,8 +0,0 @@
-export const rnd = (exclusiveMaxValue: number) => Math.floor(Math.random() * exclusiveMaxValue)
-
-export const randomElement = <T>(coll: ReadonlyArray<T>) => (coll.length > 0 ? coll[rnd(coll.length)] : undefined)
-
-export const lj = (m: string, data: any) => {
-  console.log(m, "=>", JSON.stringify(data, null, 2))
-  return data
-}
